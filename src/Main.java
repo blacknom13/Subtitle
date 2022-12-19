0f6ad2e1577000fd9497c530f5a17bfb70eb6696
ruslanalman <almandili92@gmail.com>
2022-12-19 19:42:48 +0100

    - Added a generic file chooser function. - Added the ability to locate the MPC to auto open it.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import com.eeeeeric.mpc.hc.api.TimeCodeException;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -13,11 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int connectionPort=-1;
+        int connectionPort = -1;
         String intputPort = "";
 
         String path = "";
+        String MPCPath="";
+        int returnVal;
         ArrayList<String> settings = new ArrayList<>();
+        List<Object> values;
         String temp;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(prefs))) {
@@ -30,31 +33,37 @@ public class Main {
 
         if (settings.size() > 0) {
             intputPort = settings.get(0);
-            path = settings.get(1);
+            if (settings.size()>1)
+                MPCPath=settings.get(1);
+            if (settings.size()>2)
+                path = settings.get(2);
         }
 
         while (true) {
             intputPort = JOptionPane.showInputDialog(null, "Enter the number port on which the player listens", intputPort);
             try {
-                if (intputPort==null)
+                if (intputPort == null)
                     break;
                 connectionPort = Integer.parseInt(intputPort);
+                values=activateFileChooser(MPCPath,"Locate the Media Player Classic program","exe");
+                MPCPath=(String)values.get(1);
+                Runtime.getRuntime().exec(MPCPath, null, new File(MPCPath.substring(0,MPCPath.lastIndexOf("\\"))));
                 ClassicPlayerUse.initConnection(connectionPort);
                 ClassicPlayerUse.mpc.seek(new TimeCode("00:00:00"));
                 break;
             } catch (NumberFormatException e) {
                 JOptionPane.showConfirmDialog(null, "The number format isn't correct, please retry",
-                        "Erroneous number",JOptionPane.DEFAULT_OPTION);
+                        "Erroneous number", JOptionPane.DEFAULT_OPTION);
                 intputPort = "";
-            }catch (IOException|TimeCodeException e) {
+            } catch (IOException | TimeCodeException e) {
                 JOptionPane.showConfirmDialog(null,
-                        "Either the Media Player Classic is offline or the port isn't correct.","Error",JOptionPane.DEFAULT_OPTION,
+                        "Either the Media Player Classic is offline or the port isn't correct.", "Error", JOptionPane.DEFAULT_OPTION,
                         JOptionPane.ERROR_MESSAGE);
             }
         }
 
-        if (intputPort==null) {
-            JOptionPane.showConfirmDialog(null, "The Program is terminated","Exit",JOptionPane.DEFAULT_OPTION,
+        if (intputPort == null) {
+            JOptionPane.showConfirmDialog(null, "The Program is terminated", "Exit", JOptionPane.DEFAULT_OPTION,
                     JOptionPane.INFORMATION_MESSAGE);
             System.exit(0);
         }
@@ -62,17 +71,12 @@ public class Main {
         ArrayList<String> subtitles = new ArrayList<>();
         ArrayList<String> timing = new ArrayList<>();
 
-
-        JFileChooser ch = new JFileChooser(path);
-        ch.setDialogTitle("Choose subtitle file to edit");
-        int returnVal;
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Subtitle files", "srt", "txt");
-        ch.setFileFilter(filter);
-
-        returnVal = ch.showOpenDialog(null);
+        values = activateFileChooser(path, "Choose subtitle file to edit", "srt", "txt");
+        returnVal=(int)values.get(0);
+        path=(String) values.get(1);
 
         if (returnVal == JFileChooser.APPROVE_OPTION) {
-            path = ch.getSelectedFile().getPath();
+            //path = ch.getSelectedFile().getPath();
             String tempSub = "", tempTime = "";
             File f = new File(path);
             boolean over = false;
@@ -124,35 +128,36 @@ public class Main {
         if (returnVal == JOptionPane.OK_OPTION) {
             File outfull;
             File outWorking;
-            String full="",working="";
+            String full = "", working = "";
             if (checkBox.isSelected()) {
                 if (path.substring(path.lastIndexOf('\\')).contains("work")) {
                     working = path;
-                    full="";
-                }else if (path.substring(path.lastIndexOf('\\')).contains("full")){
+                    full = "";
+                } else if (path.substring(path.lastIndexOf('\\')).contains("full")) {
                     full = path;
-                    working =path.substring(0,path.lastIndexOf('\\')+1)+
-                            path.substring(path.lastIndexOf('\\')).replaceAll("full","work");
-                }else {
-                    full = path.substring(0,path.lastIndexOf('\\')+1)+
-                            "full "+path.substring(path.lastIndexOf('\\')+1);
-                    working = path.substring(0,path.lastIndexOf('\\')+1)+
-                            "work "+path.substring(path.lastIndexOf('\\')+1);;
+                    working = path.substring(0, path.lastIndexOf('\\') + 1) +
+                            path.substring(path.lastIndexOf('\\')).replaceAll("full", "work");
+                } else {
+                    full = path.substring(0, path.lastIndexOf('\\') + 1) +
+                            "full " + path.substring(path.lastIndexOf('\\') + 1);
+                    working = path.substring(0, path.lastIndexOf('\\') + 1) +
+                            "work " + path.substring(path.lastIndexOf('\\') + 1);
+                    ;
                 }
             }
-            if (!checkBox.isSelected()){
-                ch = new JFileChooser(path);
-                ch.setFileFilter(filter);
-                ch.setDialogTitle("Save to...");
-                returnVal = ch.showSaveDialog(null);
+            if (!checkBox.isSelected()) {
+                values=activateFileChooser(path, "Save to...");
+                returnVal=(int)values.get(0);
+                path=(String)values.get(2);
+                String middleName=(String) values.get(3);
+
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    path = ch.getCurrentDirectory().getAbsolutePath();
-                    full = path + "\\full " + ch.getSelectedFile().getName() + ".srt";
-                    working =path + "\\work " + ch.getSelectedFile().getName() + ".srt";
+                    full = path + "\\full " + middleName + ".srt";
+                    working = path + "\\work " + middleName + ".srt";
                 }
             }
-            outfull=new File(full);
-            outWorking=new File(working);
+            outfull = new File(full);
+            outWorking = new File(working);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(outWorking))) {
                 for (int i = 0; i < subtitles.size(); i++) {
                     if (!timing.get(i).isEmpty()) {
@@ -180,6 +185,7 @@ public class Main {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(prefs))) {
             writer.write("" + connectionPort + "\n");
+            writer.write(MPCPath+"\n");
             writer.write(path);
         } catch (IOException e) {
             e.printStackTrace();
@@ -190,4 +196,12 @@ public class Main {
         windowClosed = true;
     }
 
+    static List<Object> activateFileChooser(String path, String title, String... fileExtentions) {
+        JFileChooser ch=new JFileChooser(path);
+        ch.setDialogTitle(title);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Subtitle files", fileExtentions);
+        ch.setFileFilter(filter);
+        return Arrays.asList(ch.showOpenDialog(null), ch.getSelectedFile().getPath(),
+                ch.getSelectedFile().getName());
+    }
 }
